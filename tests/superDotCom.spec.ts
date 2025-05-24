dee35f2e929a05a52893cdfabf88e2cfb16eca09
nyore192 <nickyore@yahoo.com>
2025-05-24 15:28:16 -0700

    TelephoneField
    TelephoneField

--- tests/superDotCom.spec.ts
+++ tests/superDotCom.spec.ts
@@ -13,10 +13,30 @@ test.describe('Super.com tests', () => {
 
     test('Click Save header link and verify page', async ({ page }) => {
         await page.goto('https://www.super.com/home/');
-        await page.locator('#desktop-header-save').click();
+
+        const saveHeader = page.locator('#desktop-header-save');
+
+        await saveHeader.click();
         await expect(page).toHaveURL('https://www.super.com/home/save');
         await expect(page.getByRole('heading', {name: "Save money everyday"})).toBeVisible();
         await expect(page.getByRole('heading', {name: "Online offers"})).toBeVisible();
         await expect(page.getByRole('heading', {name: "More ways to save"})).toBeVisible();
     })
+
+    test('Click more tab and close menu', async ({ page }) => {
+        await page.goto('https://www.super.com/home/');
+        await page.locator("#desktop-header-more").click();
+        await page.locator('data-testid=CloseIcon').click();
+    })
+
+    test('Click on Travel and enter random number', async ({ page }) => {
+        const phoneNumberInputField = page.locator('input[type="tel"]');
+
+        await page.goto('https://www.super.com/home/');
+        await page.locator('#desktop-header-travel').click();
+        await expect(page.locator('[data-test-id="phonenumber-continue-button"]')).toBeVisible();
+        await expect(phoneNumberInputField).toBeVisible();
+        //phoneNumberInputField.fill('9999999999');
+    })
+
 })
